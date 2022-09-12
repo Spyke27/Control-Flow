a8dc547e9ee524addabcfa104061931107607317
RodrigoDaniel <73563118+Spyke27@users.noreply.github.com>
2022-09-12 11:24:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+/*
+//  Mostrar o maior número
+var n1 = Number(prompt("Digite o primeiro número:"))
+var n2 = Number(prompt("Digite o segundo número:"))
+var n3 = Number(prompt("Digite o terceiro número:"))
+
+/* 
+if(n1 > n2 && n1 > n3) {
+    alert(`O ${n1} é o maior numero!`)
+}
+if(n2 > n1 && n2 > n3) {
+    alert(`O ${n2} é o maior numero!`)
+}
+if(n3 > n2 && n3 > n1) {
+    alert(`O ${n3} é o maior numero!`)
+}*/
+/*
+// Pegar 3 número e colocá-los em ordem crescente 
+var a = Number(prompt("Digite o primeiro número:"))
+var b = Number(prompt("Digite o segundo número:"))
+var c = Number(prompt("Digite o terceiro número:"))
+
+ if(a>b && b>c ){
+    alert(`A ordem é ${a} > ${b} > ${c}`)
+}
+ else if(a>b && a>c && c>b ){
+    alert(`A ordem é ${a} > ${c} > ${b}`)
+}
+ if(b>a && a>c){
+    alert(`A ordem é ${b} > ${a} > ${c}`)
+}
+ else if(b>a && c>a && b>c){
+    alert(`A ordem é ${b} > ${c} > ${a}`)
+}
+ if(c>a && a>b){
+    alert(`A ordem é ${c} > ${a} > ${b}`)
+}
+ else if(c>a && b>a && c>b){
+    alert(`A ordem é ${c} > ${b} > ${a}`)
+}
+*/
+/*
+// Dizendo a faixa etária do usuario de acordo com sua idade
+var idade = Number(prompt("Digite sua idade:"))
+
+if(idade < 14){
+    alert("Você é criança!")
+}
+else if(idade >= 15 && idade <= 17){
+    alert("Você é adolescente!")
+}
+else if(idade >= 18){
+    alert("Você é adulto!")
+}
+*/
+/*
+// Verificando se é par ou ímpar e mostrar a raiz ou elevação ao quadrado
+var num = Number(prompt("Digite um número:"))
+
+if(num % 2 == 0){
+    alert(`O número é par!\n E a raiz quadrada de ${num} é ${Math.sqrt(num)}`)
+}
+else{
+    alert(`O número é ímpar!\n E ${num} elevado ao quadrado é ${num**2}`)
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
